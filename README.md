e42f7f34a46289823e912d1e7a8f30d5c33008ac
Sehan Kim <sehandev@gmail.com>
2022-01-17 16:35:39 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<div align="center">
+
+## PyTorch Lightning Template
+
+</div>
+
+Get W&B API key from https://wandb.ai/authorize
+
+```bash
+pip install -r requirements.txt
+wandb login
+```
---
+++ data_module.py
@@ -0,0 +1,62 @@
+# Standard
+
+# PIP
+from torch.utils.data import DataLoader
+from pytorch_lightning import LightningDataModule
+from pl_bolts.datasets import DummyDataset
+
+# Custom
+from custom.dataset import CustomDataset
+
+
+class CustomDataModule(LightningDataModule):
+    def __init__(
+        self,
+        seq_len,
+        batch_size=1,
+        num_workers=0,
+    ):
+        super().__init__()
+        self.seq_len = seq_len
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        self.set_datasets()
+
+    def set_datasets(self):
+        # self.train_dataset = CustomDataset(
+        #     seq_len=self.seq_len,
+        # )
+        # self.valid_dataset = CustomDataset(
+        #     seq_len=self.seq_len,
+        # )
+        # self.test_dataset = CustomDataset(
+        #     seq_len=self.seq_len,
+        # )
+        self.train_dataset = DummyDataset((1, 28, 28), (1,))
+        self.valid_dataset = DummyDataset((1, 28, 28), (1,))
+        self.test_dataset = DummyDataset((1, 28, 28), (1,))
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.valid_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
