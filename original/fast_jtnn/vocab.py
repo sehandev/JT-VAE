00ae137405cc19e97dfd01b776c8b13ef17f252d
sehandev <sehandev@gmail.com>
2022-01-18 16:00:08 +0900

    REFACTOR vocab.py

--- original/fast_jtnn/vocab.py
+++ original/fast_jtnn/vocab.py
@@ -1,22 +1,51 @@
-import rdkit
-import rdkit.Chem as Chem
+# Standard
 import copy
 
-def get_slots(smiles):
-    mol = Chem.MolFromSmiles(smiles)
-    return [(atom.GetSymbol(), atom.GetFormalCharge(), atom.GetTotalNumHs()) for atom in mol.GetAtoms()]
+# 3rd Party
+from rdkit.Chem import MolFromSmiles
 
-class Vocab(object):
-    benzynes = ['C1=CC=CC=C1', 'C1=CC=NC=C1', 'C1=CC=NN=C1', 'C1=CN=CC=N1', 'C1=CN=CN=C1', 'C1=CN=NC=N1', 'C1=CN=NN=C1', 'C1=NC=NC=N1', 'C1=NN=CN=N1']
+
+class Vocab:
+    """
+    Parameters:
+        penzynes (list): list of 5-membered ring
+        benzynes (list): list of 6-membered ring
+    """
     penzynes = ['C1=C[NH]C=C1', 'C1=C[NH]C=N1', 'C1=C[NH]N=C1', 'C1=C[NH]N=N1', 'C1=COC=C1', 'C1=COC=N1', 'C1=CON=C1', 'C1=CSC=C1', 'C1=CSC=N1', 'C1=CSN=C1', 'C1=CSN=N1', 'C1=NN=C[NH]1', 'C1=NN=CO1', 'C1=NN=CS1', 'C1=N[NH]C=N1', 'C1=N[NH]N=C1', 'C1=N[NH]N=N1', 'C1=NN=N[NH]1', 'C1=NN=NS1', 'C1=NOC=N1', 'C1=NON=C1', 'C1=NSC=N1', 'C1=NSN=C1']
+    benzynes = ['C1=CC=CC=C1', 'C1=CC=NC=C1', 'C1=CC=NN=C1', 'C1=CN=CC=N1', 'C1=CN=CN=C1', 'C1=CN=NC=N1', 'C1=CN=NN=C1', 'C1=NC=NC=N1', 'C1=NN=CN=N1']
 
     def __init__(self, smiles_list):
         self.vocab = smiles_list
-        self.vmap = {x:i for i,x in enumerate(self.vocab)}
-        self.slots = [get_slots(smiles) for smiles in self.vocab]
-        Vocab.benzynes = [s for s in smiles_list if s.count('=') >= 2 and Chem.MolFromSmiles(s).GetNumAtoms() == 6] + ['C1=CCNCC1']
-        Vocab.penzynes = [s for s in smiles_list if s.count('=') >= 2 and Chem.MolFromSmiles(s).GetNumAtoms() == 5] + ['C1=NCCN1','C1=NNCC1']
-        
+        self.vmap = {x: i for i, x in enumerate(smiles_list)}
+        self.slot_list = [self.get_slot(smiles) for smiles in smiles_list]
+
+        self.update_ring_list()
+
+    @staticmethod
+    def get_slot(smiles):
+        mol = MolFromSmiles(smiles)
+
+        slot = []
+        for atom in mol.GetAtoms():
+            slot.append((atom.GetSymbol(), atom.GetFormalCharge(), atom.GetTotalNumHs()))
+
+        return slot
+
+    def update_ring_list(self):
+        ring_5_list = ['C1=NCCN1', 'C1=NNCC1']
+        ring_6_list = ['C1=CCNCC1']
+
+        for smiles in self.vocab:
+            if smiles.count('=') >= 2:
+                num_atoms = MolFromSmiles(smiles).GetNumAtoms()
+                if num_atoms == 5:
+                    ring_5_list.append(smiles)
+                elif num_atoms == 6:
+                    ring_6_list.append(smiles)
+
+        Vocab.penzynes = ring_5_list
+        Vocab.benzynes = ring_6_list
+
     def get_index(self, smiles):
         return self.vmap[smiles]
 
@@ -24,8 +53,7 @@ class Vocab(object):
         return self.vocab[idx]
 
     def get_slots(self, idx):
-        return copy.deepcopy(self.slots[idx])
+        return copy.deepcopy(self.slot_list[idx])
 
     def size(self):
         return len(self.vocab)
-
